abc31b296720b8052fb9194e60d3cc7ed9ee3b99
Vikash Sharma <82925579+vikash28-cloud@users.noreply.github.com>
2024-05-03 07:41:21 +0000

    adding node mailer

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const { registerUser, loginUser, logout } = require("../controllers/userController")
+const { registerUser, loginUser, logout, forgetPassword } = require("../controllers/userController")
 
 router.route("/register").post(registerUser);
 router.route("/login").post(loginUser);
+router.route("/password/forgetPassword").post(forgetPassword);
 router.route("/logout").get(logout)
 module.exports = router;
