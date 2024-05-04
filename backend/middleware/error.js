07cab98d3869ad0c18b340defc0de66e1b8e2736
Vikash Sharma <82925579+vikash28-cloud@users.noreply.github.com>
2024-05-04 09:49:32 +0000

    Backend Authentication is Done

--- backend/middleware/error.js
+++ backend/middleware/error.js
@@ -5,13 +5,30 @@ module.exports = (err, req, res, next) => {
     err.statusCode = err.statusCode || 500;
     err.message = err.message || "internal server error"
 
-    // wrong mongodb id error
+    // wrong mongodb id error 
     if (err.name === "CastError") {
         const message = `resource not found ${err.path} `
         err = new ErrorHandler(message, 400);
     }
 
+    // mongoose duplicate key error
+    if(err.code === 11000){
+        const message = `Duplicate ${Object.keys(err.keyValue)} Entered`
+        err = new ErrorHandler(message,400);
 
+    }
+
+    // wrong JWT Error
+    if(err.name == 'JsonWebTokenError'){
+        const message = `Json Web Token is invalid Please Try again`
+        err = new ErrorHandler(message,400);
+    }
+
+    // JWT Expire Error
+    if(err.name == 'TokenExpiredError'){
+        const message = `Json Web Token is Expired, Try again`
+        err = new ErrorHandler(message,400);
+    }
 
     res.status(err.statusCode).json({
         success: false,
