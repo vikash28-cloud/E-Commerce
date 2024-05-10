4e662cd971d8d642b2f3cdcd15396cd516e2fd19
Vikash Sharma <82925579+vikash28-cloud@users.noreply.github.com>
2024-05-10 11:49:20 +0000

    Created Order model

---
+++ backend/models/orderModel.js
@@ -0,0 +1,108 @@
+const mongoose = require("mongoose");
+
+const orderSchema = new mongoose.Schema({
+    shippingInfo: {
+        address: {
+            type: String,
+            required: true,
+        },
+        city: {
+            type: String,
+            required: true,
+        },
+        state: {
+            type: String,
+            required: true,
+        },
+        country: {
+            type: String,
+            required: true,
+        },
+        pinCode: {
+            type: Number,
+            required: true,
+        },
+        phoneNo: {
+            type: Number,
+            required: true,
+        },
+    },
+    OrderItems: [
+        {
+            name: {
+                type: String,
+                required: true,
+            },
+            quantity: {
+                type: Number,
+                required: true,
+            },
+            price: {
+                type: Number,
+                required: true,
+            },
+            image: {
+                type: String,
+                required: true,
+            },
+            product: {                           //productID of the item that is purchasing is also stored
+                type: mongoose.Schema.ObjectId,
+                ref: "Product",
+                required: true
+            }
+
+        },
+    ],
+    user: {
+        type: mongoose.Schema.ObjectId,
+        ref: "User",
+        required: true
+    },
+    paymentInfo: {
+        id: {
+            type: String,
+            required: true,
+        },
+        status: {
+            type: String,
+            required: true,
+        }
+    },
+    paidAt: {
+        type: Date,
+        required: true
+    },
+    itemsPrice: {
+        type: Number,
+        default: 0,
+        required: true,
+    },
+    taxPrice: {
+        type: Number,
+        default: 0,
+        required: true,
+    },
+    shippingPrice: {
+        type: Number,
+        default: 0,
+        required: true,
+    },
+    totalPrice: {
+        type: Number,
+        default: 0,
+        required: true,
+    },
+    orderStatus:{
+        type:String,
+        required:true,
+        default:"Processing"
+    },
+    deliveredAt:Date,
+    type:Date,
+    default:Date.now()
+
+});
+
+const Order = mongoose.model("Order",orderSchema);
+
+module.exports = Order;
