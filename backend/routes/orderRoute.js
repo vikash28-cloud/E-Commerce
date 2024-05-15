e503209d797a7bb2dcdf389465588250dfeda5f0
Vikash Sharma <82925579+vikash28-cloud@users.noreply.github.com>
2024-05-15 05:33:11 +0000

    Pending changes exported from your codespace

--- backend/routes/orderRoute.js
+++ backend/routes/orderRoute.js
@@ -2,10 +2,25 @@ const express = require("express");
 const router = express.Router();
 
 const { isAuthenticatedUser, authorizeRole } = require("../middleware/Auth");
-const { newOrder, getSingleOrder, myOrders } = require("../controllers/orderController");
+const {
+  newOrder,
+  getSingleOrder,
+  myOrders,
+  GetAllOrders,
+  updateOrder,
+  deleteOrder,
+} = require("../controllers/orderController");
 
-router.route("/order/new").post(isAuthenticatedUser,newOrder);
-router.route("/order/:id").get(isAuthenticatedUser,authorizeRole("admin"),getSingleOrder)
-router.route("/orders/me").get(isAuthenticatedUser,myOrders); 
+router.route("/order/new").post(isAuthenticatedUser, newOrder);
+router.route("/order/:id").get(isAuthenticatedUser, getSingleOrder);
+router.route("/orders/me").get(isAuthenticatedUser, myOrders);
+router
+  .route("/admin/orders")
+  .get(isAuthenticatedUser, authorizeRole("admin"), GetAllOrders);
 
-module.exports = router
+router
+  .route("/admin/order/:id")
+  .put(isAuthenticatedUser, authorizeRole("admin"), updateOrder)
+  .delete(isAuthenticatedUser, authorizeRole("admin"), deleteOrder);
+
+module.exports = router;
