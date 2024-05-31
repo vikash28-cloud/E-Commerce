753d7b4f9d060445bc2155e535c31daa2ca61d11
Vikash Sharma <expertvikash@gmail.com>
2024-05-31 13:21:11 +0530

    change .env path

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ const app = require("./app");
 const dotenv = require('dotenv');
 const connectDatabase = require("./config/database")
 // config
-dotenv.config({path:"backend/config/config.env"});
+dotenv.config({path:"config/config.env"});
 
 // connect database
 connectDatabase();
